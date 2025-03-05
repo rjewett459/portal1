433fb80222362eeb7c195cd02093754798d85d35
ChatSites™ <info@chatsites.io>
2025-03-05 13:13:50 -0700

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,8 @@ app.get("/token", async (req, res) => {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          model: "gpt-4o-realtime-preview-2024-12-17",
+          model: "gpt-4o-mini-realtime-preview-2024-12-17
+",
           voice: "verse",
         }),
       },
